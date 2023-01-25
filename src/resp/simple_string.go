53e149f22a63e865ebf19a00c158650c0b7072b6
Jan Carreras <contact@jcarreras.es>
2023-01-25 18:02:23 +0100

    Support nullable Simple Strings
    It was unclear from the specification that this could be the case, but the null value of a simple string is "$-1\r\n" and we must supported.
    
    We're leaving the implementation of parsing a null string for the future

--- src/resp/simple_string.go
+++ src/resp/simple_string.go
@@ -9,6 +9,7 @@ var _ dataType = (*SimpleString)(nil)
 // SimpleString returns a SimpleString datatype
 type SimpleString struct {
 	string string
+	isNull bool
 }
 
 // NewSimpleString returns a SingleString datatype
@@ -16,15 +17,24 @@ func NewSimpleString(string string) *SimpleString {
 	return &SimpleString{string: string}
 }
 
+// NewNullSimpleString Returns a null Simple String, represented in the network as "$-1\r\n"
+func NewNullSimpleString() *SimpleString {
+	return &SimpleString{isNull: true}
+}
+
 // ReadFrom reads from the Reader and loads the SimpleString object
 // Example: "+OK\r\n"
 func (s *SimpleString) ReadFrom(r io.Reader) (c int64, err error) {
+	// TODO: We don't know how to read null strings!
 	c, s.string, err = readFrom(r)
 	return c, err
 }
 
 // WriteTo writes the information on SimpleString and dumps it into the Writer
 func (s *SimpleString) WriteTo(w io.Writer) (int64, error) {
+	if s.isNull {
+		return fprintf(w, "%c-1\r\n", byte(SimpleStringOp))
+	}
 	return fprintf(w, "%c%s\r\n", byte(SimpleStringOp), s.string)
 }
 
