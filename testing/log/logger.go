576695a5a39b603b5b19a65822e03b4a14bc4aa1
Jan Carreras <contact@jcarreras.es>
2023-01-26 17:00:57 +0100

    Remove useless logger function

--- testing/log/logger.go
+++ testing/log/logger.go
@@ -8,14 +8,5 @@ import (
 
 // ServerLogger returns a logger to be used with the Server
 func ServerLogger() *log.Logger {
-	return log.New(loggerOutput(), "[server] ", 0)
-}
-
-// ClientLogger returns a logger to be used with the Client
-func ClientLogger() *log.Logger {
-	return log.New(loggerOutput(), "[client] ", 0)
-}
-
-func loggerOutput() io.Writer {
-	return io.Discard
+	return log.New(io.Discard, "[server] ", 0)
 }
